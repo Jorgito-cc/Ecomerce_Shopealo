e698b738280455dd86d7aa8d7d6dd078753cee5c
jorgito-cc <jorgitochoque007@gmail.com>
2025-10-09 01:40:25 -0400

    feat(admin):compoente de verificacion de bitacora

--- src/features/admin/pages/BitacoraPage.tsx
+++ src/features/admin/pages/BitacoraPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { getBitacoraLogs } from "../../../api/bitacora";
 import type { BitacoraLog } from "../../../types/bitacora";
 
