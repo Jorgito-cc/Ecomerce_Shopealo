375ada415ce55e57e903268c07f1f9176d1102b1
jorgito-cc <jorgitochoque007@gmail.com>
2025-10-09 01:51:40 -0400

    fix (admin):correccion de seguirdad de copoentes

--- src/api/bitacora.ts
+++ src/api/bitacora.ts
@@ -15,7 +15,12 @@ import type { BitacoraLog } from "../types/bitacora";
 
 
 // getbitacora
+/* export const getBitacoraLogs = async (password: string): Promise<BitacoraLog[]> => {
+  const { data } = await http.post<BitacoraLog[]>("/api/v1/bitacora", { password });
+  return data;
+};
+ */
 export const getBitacoraLogs = async (password: string): Promise<BitacoraLog[]> => {
-  const { data } = await http.post<BitacoraLog[]>("/api/v1/bitacora/listar", { password });
+  const { data } = await http.get<BitacoraLog[]>(`/api/v1/bitacora?password=${password}`);
   return data;
 };
